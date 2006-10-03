71d4c4cecdc116b0c301b3c52603dffa280facf0
ahunter <ahunter>
2006-10-03 15:02:57 +0000

    gmf_head ahunter 061003 Merge 1.0.1 Runtime to HEAD 2.0

---
+++ org.eclipse.gmf.runtime/tests/org.eclipse.gmf.tests.runtime.emf.type.core/src/org/eclipse/gmf/tests/runtime/emf/type/core/employee/HighSchoolStudent.java
@@ -0,0 +1,21 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.eclipse.gmf.tests.runtime.emf.type.core.employee;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>High School Student</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see org.eclipse.gmf.tests.runtime.emf.type.core.employee.EmployeePackage#getHighSchoolStudent()
+ * @model
+ * @generated
+ */
+public interface HighSchoolStudent extends Student {
+} // HighSchoolStudent
